ba4426dd7ae690d375db93557ec8862b7d14599a
Habte Selassie <habtuchelsea1000@gmail.com>
2023-08-19 15:49:21 +0300

    add Navigation_Bar.jsx into Components Folder

---
+++ frontend/src/components/navigation_bar/Navigation_Bar.jsx
@@ -0,0 +1,7 @@
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { faCircle } from '@fortawesome/free-regular-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
