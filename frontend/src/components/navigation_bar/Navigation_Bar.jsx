70f016e77eea49cdf9df6a7d62e6f8489cb7a6d3
Habte Selassie <habtuchelsea1000@gmail.com>
2023-08-19 16:07:53 +0300

    adding the logo image for the navigation bar

--- frontend/src/components/navigation_bar/Navigation_Bar.jsx
+++ frontend/src/components/navigation_bar/Navigation_Bar.jsx
@@ -5,3 +5,30 @@ import { useSelector } from 'react-redux';
 import { faCircle } from '@fortawesome/free-regular-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
+function Navigation_Bar() {
+    const status = useSelector((state)=>state.database.status)
+
+    return (
+        <div>
+            <Navbar bg="blue" variant="blue">
+                <Navbar.Brand href="/">
+                <img src="/resources/images/logo.png" alt="logo" width={70} height={30} className="mr-3" />
+                  AGE Viewer
+                </Navbar.Brand>
+                <Nav className="ml-auto">
+                    <div className="text-white text-lg">
+                        Connection Stat:
+                        <div className='d-inline ml-2 text-lg-center'>
+                            {status === 'connected' ? <FontAwesomeIcon className="mr-1" icon={faCircle} color="green" style={{ fontSize: '0.60rem' }} /> : ''}
+                             {status}
+                        </div>
+                    </div>
+                </Nav>
+            </Navbar>
+        </div>
+    )
+
+
+}
+
+export default Navigation_Bar
